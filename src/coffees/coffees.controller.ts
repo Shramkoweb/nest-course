8058cf8d3e6d34e47078706520c0301ea4f6312e
Serhii Shramko <shramko.web@yahoo.com>
2022-01-04 22:54:46 +0200

    feat(controller): add CRUD to coffee controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+  @HttpCode(HttpStatus.OK)
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit ${limit}, offset: ${offset}`;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
