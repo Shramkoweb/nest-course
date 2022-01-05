2717e2393457f93a1cb18cea7b9e1b68d43ac651
Serhii Shramko <shramko.web@yahoo.com>
2022-01-06 00:44:13 +0200

    refactor: add dto for coffee entities
    add validation pipeline

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,6 +11,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from "./dto/create-coffee.dto";
+import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
 
 @Controller('coffees')
 export class CoffeesController {
@@ -31,14 +33,14 @@ export class CoffeesController {
 
   @HttpCode(HttpStatus.CREATED)
   @Post()
-  create(@Body() body) {
-    return body;
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.coffeesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
