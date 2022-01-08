b42eceef227513f12a60c79de77e809174d5f272
Serhii Shramko <shramko.web@yahoo.com>
2022-01-08 21:17:40 +0200

    test: e2e temp fix
    add sqlite for tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppModule } from '../src/app.module';
+import { CoffeesModule } from '../src/coffees/coffees.module';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        ConfigModule.forRoot(),
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: 'database.db',
+          synchronize: true,
+          logging: false,
+          autoLoadEntities: true,
+        }),
+      ],
+      controllers: [AppController],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
