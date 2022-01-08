2fa945583e7bb5bef7efe11542551295084890c4
Serhii Shramko <shramko.web@yahoo.com>
2022-01-08 19:37:07 +0200

    test: temp fix mock DB repositories

--- src/coffees/coffees.controller.spec.ts
+++ src/coffees/coffees.controller.spec.ts
@@ -2,6 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+// TODO move to some test config init ?
+class CoffeeRepositoryFake {}
+
+class FlavorRepositoryFake {}
 
 describe('CoffeesController', () => {
   let controller: CoffeesController;
@@ -9,7 +17,17 @@ describe('CoffeesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CoffeesController],
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        {
+          provide: getRepositoryToken(Coffee),
+          useClass: CoffeeRepositoryFake,
+        },
+        {
+          provide: getRepositoryToken(Flavor),
+          useClass: FlavorRepositoryFake,
+        },
+      ],
     }).compile();
 
     controller = module.get<CoffeesController>(CoffeesController);
--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,13 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { CoffeesService } from './coffees.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+
+class CoffeeRepositoryFake {}
+
+class FlavorRepositoryFake {}
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        {
+          provide: getRepositoryToken(Coffee),
+          useClass: CoffeeRepositoryFake,
+        },
+        {
+          provide: getRepositoryToken(Flavor),
+          useClass: FlavorRepositoryFake,
+        },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
