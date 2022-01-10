a4811c7aacb740821c6e0561fdd03aaf5ef91471
Serhii Shramko <shramko.web@yahoo.com>
2022-01-10 23:44:33 +0200

    Revert "test: e2e temp fix"
    This reverts commit b42eceef227513f12a60c79de77e809174d5f272.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,31 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { CoffeesModule } from '../src/coffees/coffees.module';
-import { AppController } from '../src/app.controller';
-import { AppService } from '../src/app.service';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot(),
-        CoffeesModule,
-        TypeOrmModule.forRoot({
-          type: 'sqlite',
-          database: 'database.db',
-          synchronize: true,
-          logging: false,
-          autoLoadEntities: true,
-        }),
-      ],
-      controllers: [AppController],
-      providers: [AppService],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
