26d13f700ff97d5c812ecd8b291996d8e7083457
Serhii Shramko <shramko.web@yahoo.com>
2022-01-11 23:01:22 +0200

    chore: add joi validation to env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CoffeesModule, TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_USER: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_NAME: Joi.required(),
+      }),
+    }),
+    CoffeesModule,
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
