86c69efd3049fdf6392f8443e578852b3ed6200e
Serhii Shramko <shramko.web@yahoo.com>
2022-01-12 02:23:35 +0200

    feat: add ApiKeyGuard

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,8 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard());
+
   await app.listen(process.env.APP_PORT);
 }
 
